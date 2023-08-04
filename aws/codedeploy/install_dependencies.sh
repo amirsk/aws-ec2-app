532206a04a9a60d50c1f40aaed5a0f5aab84efdc
Amir Sezavar Keshavarz <1188298+amirsk@users.noreply.github.com>
2023-08-04 15:48:44 +0100

    Fixed CodeDeploy

--- aws/codedeploy/install_dependencies.sh
+++ aws/codedeploy/install_dependencies.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # Script to install dependencies for your Java application
-# For example: install Java runtime
 
 # Install Java 11 Amazon Corretto
 sudo yum install java-17-amazon-corretto -y
--- aws/codedeploy/start_application.sh
+++ aws/codedeploy/start_application.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-# Script to start your Java application
-# Replace "demo.jar" with the actual JAR file name
+APP_DIR=/home/ec2-user/app
+APP_LOG_DIR=/home/ec2-user/app/log
 
-java -jar ./demo.jar > ./application.log 2>&1 &
+mkdir -p ${APP_LOG_DIR}
+chmod 755 ${APP_LOG_DIR}/
+touch ${APP_LOG_DIR}/app.log
+chmod 644 ${APP_LOG_DIR}/app.log
+chown -R ec2-user ${APP_LOG_DIR}
+chown -R ec2-user ${APP_DIR}
+
+su - ec2-user -c "java -jar ${APP_DIR}/demo.jar > ${APP_LOG_DIR}/app.log 2>&1 &"
+
+exit 0
